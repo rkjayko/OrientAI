bf44df59395ab2e5ecc29b8c4f39bd743896add8
Jaime Mejia <jadmejia@bancolombia.com.co>
2025-08-01 17:40:57 -0500

    se añade ci-cd y test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-// vite.config.js
+/// <reference types="vitest" />
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -8,6 +8,11 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.js',
+    },
     server: {
       proxy: {
         '/api': {
